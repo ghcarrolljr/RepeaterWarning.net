bfe38ce82d691f284a64952bed785ac13dd8c04e
George Carroll <ghcarrolljr@gmail.com>
2025-01-28 07:33:04 -0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -129,15 +129,15 @@ function writeCommonOptions() {
 <?php
 
 makeDrawChart("Battery (v)", "chart1a", "options.battery", "jsonData.repeaters.entries['W5AUU-1'].voltage/10",0);
-makeDrawChart("Time (min)", "chart1b", "options.time", "jsonData.repeaters.entries['W5AUU-1'].lasttime",0);
+makeDrawChart("Time (min)", "chart1b", "options.time", "jsonData.repeaters.entries['W5AUU-1'].lastStatusTime",0);
 makeDrawChart("Grid power", "chart1c", "options.power", "jsonData.repeaters.entries['W5AUU-1'].powerValueForCharts",3);
 
 makeDrawChart("Battery (v)", "chart2a", "options.battery", "jsonData.repeaters.entries['W5AUU-2'].voltage/10",0);
-makeDrawChart("Time (min)", "chart2b", "options.time", "jsonData.repeaters.entries['W5AUU-2'].lasttime",0);
+makeDrawChart("Time (min)", "chart2b", "options.time", "jsonData.repeaters.entries['W5AUU-2'].lastStatusTime",0);
 makeDrawChart("Grid power", "chart2c", "options.power", "jsonData.repeaters.entries['W5AUU-2'].powerValueForCharts",3);
 
 makeDrawChart("Battery (v)", "chart3a", "options.battery", "jsonData.repeaters.entries['W5AUU-3'].voltage/10",0);
-makeDrawChart("Time (min)", "chart3b", "options.time", "jsonData.repeaters.entries['W5AUU-3'].lasttime",0);
+makeDrawChart("Time (min)", "chart3b", "options.time", "jsonData.repeaters.entries['W5AUU-3'].lastStatusTime",0);
 makeDrawChart("Grid power", "chart3c", "options.power", "jsonData.repeaters.entries['W5AUU-3'].powerValueForCharts",3);
 
 ?>
