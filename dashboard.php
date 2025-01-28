85a587419dec7c24ca2ac202ce9e0502b9238e6c
George Carroll <ghcarrolljr@gmail.com>
2025-01-28 08:01:52 -0600

    Update dashboard.php
    conver lasttime

--- dashboard.php
+++ dashboard.php
@@ -129,7 +129,7 @@ function writeCommonOptions() {
 <?php
 
 makeDrawChart("Battery (v)", "chart1a", "options.battery", "jsonData.repeaters.entries['W5AUU-1'].voltage/10",0);
-makeDrawChart("Time (min)", "chart1b", "options.time", "jsonData.repeaters.entries['W5AUU-1'].lastStatusTime",0);
+makeDrawChart("Time (min)", "chart1b", "options.time", "jsonData.repeaters.entries['W5AUU-1'].lastStatusTime/22000",0);
 makeDrawChart("Grid power", "chart1c", "options.power", "jsonData.repeaters.entries['W5AUU-1'].gridPower",3);
 
 makeDrawChart("Battery (v)", "chart2a", "options.battery", "jsonData.repeaters.entries['W5AUU-2'].voltage/10",0);
