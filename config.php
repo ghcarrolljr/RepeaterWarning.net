96d3493e412f032db679bb85f6e7dfe52b6f7904
George Carroll <ghcarrolljr@gmail.com>
2025-01-29 08:28:19 -0600

    Update config.php
    Changed my text number

--- config.php
+++ config.php
@@ -13,11 +13,11 @@ class database {
 
 // Not used but might need to be if you want to alert different ppl for different problems.
 $recipients = [
-        "typicalSuspects" => "joshcarroll21@gmail.com, 5015140605@vzwpix.com ",//, 5012404779@txt.att.net, 5015487682@vtext.com, 5012691864@vtext.com, 5014999595@google", vzwpix.com
+        "typicalSuspects" => "joshcarroll21@gmail.com, 5015140605txtfirstnet-mail.com ",//, 5012404779@txt.att.net, 5015487682@vtext.com, 5012691864@vtext.com, 5014999595@google", vzwpix.com
         "w5auu1Power" => "",
         "w5auu2Power" => "",//5014725646@txt.att.net",
         "w5auu3Power" => "",
-        "testMode" => "joshcarroll21@gmail.com, 5015140605@vzwpix.com"
+        "testMode" => "joshcarroll21@gmail.com, 5015140605@txt.firstnet-mail.com"
 ];
 
 $pushover_token = "a421kzttscw9qk3wkc2bew4ok6y118";
